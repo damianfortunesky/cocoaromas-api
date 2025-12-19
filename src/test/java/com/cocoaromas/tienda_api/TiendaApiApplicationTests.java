0d98e990f13842a0cf6d088635b2017854603c03
unknown <damianfortunesky@gmail.com>
2025-12-18 21:13:00 -0300

    Se crea proyecto con  springboot

---
+++ src/test/java/com/cocoaromas/tienda_api/TiendaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocoaromas.tienda_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
