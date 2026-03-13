19232c62f5bdd9eea2697ddd1cb9e0c9157c1bb6
Damian Fortunesky <80221328+damianfortunesky@users.noreply.github.com>
2026-03-13 04:26:26 -0300

    Refactor user auth model and remove DB seed data

--- src/main/java/com/cocoaromas/api/domain/auth/RegisterCommand.java
+++ src/main/java/com/cocoaromas/api/domain/auth/RegisterCommand.java
@@ -1,4 +1,4 @@
 package com.cocoaromas.api.domain.auth;
 
-public record RegisterCommand(String firstName, String lastName, String email, String password) {
+public record RegisterCommand(String email, String password) {
 }
--- src/main/java/com/cocoaromas/api/domain/auth/RegisteredUser.java
+++ src/main/java/com/cocoaromas/api/domain/auth/RegisteredUser.java
@@ -4,8 +4,6 @@ import java.time.OffsetDateTime;
 
 public record RegisteredUser(
         Long id,
-        String firstName,
-        String lastName,
         String email,
         Role role,
         OffsetDateTime createdAt
