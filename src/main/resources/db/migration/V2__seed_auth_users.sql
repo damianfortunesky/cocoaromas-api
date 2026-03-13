1717dd776e9e759bab6ba60e42c835f83e676e04
unknown <damianfortunesky@gmail.com>
2026-03-13 01:22:50 -0300

    configuracion variables de entorno y db

--- src/main/resources/db/migration/V2__seed_auth_users.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO users (name, email, username, password_hash, role)
-VALUES
-('Admin User', 'admin@cocoaromas.local', 'admin', '{noop}Admin123!', 'ADMIN'),
-('Owner User', 'owner@cocoaromas.local', 'owner', '{noop}Owner123!', 'OWNER'),
-('Employee User', 'employee@cocoaromas.local', 'employee', '{noop}Employee123!', 'EMPLOYEE'),
-('Client User', 'client@cocoaromas.local', 'client', '{noop}Client123!', 'CLIENT');
