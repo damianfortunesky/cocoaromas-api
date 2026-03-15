095b661a381b835d04f0c1b2488ee51d8f0f3095
Damian Fortunesky <80221328+damianfortunesky@users.noreply.github.com>
2026-03-15 05:13:45 -0300

    Harden category description migrations to avoid startup conflicts

--- src/main/resources/db/migration/V12__add_column_categories.sql
+++ src/main/resources/db/migration/V12__add_column_categories.sql
@@ -1,2 +1,6 @@
-ALTER TABLE categories
-ADD description VARCHAR(500) NULL;
+IF OBJECT_ID('dbo.categories', 'U') IS NOT NULL
+   AND COL_LENGTH('dbo.categories', 'description') IS NULL
+BEGIN
+    ALTER TABLE dbo.categories
+    ADD description NVARCHAR(500) NULL;
+END;
